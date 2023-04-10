b523a4e1d93bc5f5a90bb4bd27ecd984c858f5c8
RomanAbdalov <117114038+RomanAbdalov@users.noreply.github.com>
2023-04-10 16:27:05 +0300

    add js files to task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,40 @@
+const refs = {
+    btnStart:  document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
+    bodyEl: document.querySelector('body')    
+};
+console.log(refs.btnStart);
+console.log(refs.btnStop);
+
+refs.btnStart.addEventListener('click', () => {
+    const srartRandomColor = setInterval(getRandomHexColor(), 1000);
+    refs.bodyEl.style.backgroundColor = getRandomHexColor();
+    console.log(getRandomHexColor())
+});
+refs.btnStop.addEventListener('click', stopChangeColor);
+
+// let chengeColorId = setInterval(getRandomHexColor(), 1000);
+// console.log(chengeColorId);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
+
+function startChangeColor(){
+  
+    refs.btnStart.disabled = true;
+    refs.btnStop.disabled = false;
+
+};
+
+function stopChangeColor(){
+    clearInterval(chengeColorId);
+    refs.btnStart.disabled = false;
+    refs.btnStop.disabled = true;
+      
+};
+
+
+
 
