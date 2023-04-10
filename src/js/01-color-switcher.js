dc026382f86ac7ab21d4fb98198aee7eaaa2b6c9
RomanAbdalov <117114038+RomanAbdalov@users.noreply.github.com>
2023-04-10 16:55:25 +0300

    finish task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,35 +6,28 @@ const refs = {
 console.log(refs.btnStart);
 console.log(refs.btnStop);
 
-refs.btnStart.addEventListener('click', () => {
-    const srartRandomColor = setInterval(getRandomHexColor(), 1000);
-    refs.bodyEl.style.backgroundColor = getRandomHexColor();
-    console.log(getRandomHexColor())
-});
+refs.btnStart.addEventListener('click', startChangeColor);
 refs.btnStop.addEventListener('click', stopChangeColor);
 
-// let chengeColorId = setInterval(getRandomHexColor(), 1000);
-// console.log(chengeColorId);
-
-function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-};
-
-
-function startChangeColor(){
-  
+function startChangeColor() {
+    timerId = setInterval(() => {
+        refs.bodyEl.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+     
     refs.btnStart.disabled = true;
     refs.btnStop.disabled = false;
-
 };
 
 function stopChangeColor(){
-    clearInterval(chengeColorId);
+    clearInterval(timerId);
     refs.btnStart.disabled = false;
     refs.btnStop.disabled = true;
       
 };
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
 
 
 
